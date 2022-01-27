bc189f8ee082bca7247d3e57a0325c4f41b62ac0
Emil Severin Ramsvik <emilseverinramsvik@Emils-MacBook-Pro.local>
2022-01-27 18:34:06 +0100

    format tf files

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,19 +1,19 @@
-variable path {
+variable "path" {
   default = "/Users/emilseverinramsvik/Documents/twitter-note-publisher/infra/"
 }
 
-variable AIRTABLE_API_KEY {
-sensitive = true    
+variable "AIRTABLE_API_KEY" {
+  sensitive = true
 }
-variable TWITTER_ACCESS_TOKEN_SECRET {
-sensitive = true    
+variable "TWITTER_ACCESS_TOKEN_SECRET" {
+  sensitive = true
 }
-variable TWITTER_ACCESS_TOKEN {
-sensitive = true    
+variable "TWITTER_ACCESS_TOKEN" {
+  sensitive = true
 }
-variable TWITTER_CONSUMER_SECRET {
-sensitive = true    
+variable "TWITTER_CONSUMER_SECRET" {
+  sensitive = true
 }
-variable TWITTER_CONSUMER_KEY {
-sensitive = true    
+variable "TWITTER_CONSUMER_KEY" {
+  sensitive = true
 }
