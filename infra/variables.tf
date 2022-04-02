04eec011ff0f25e09c9c1a1605ad281f12422aca
Emil Ramsvik <emilseverin93@gmail.com>
2022-04-02 12:28:14 +0200

    added description of variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,22 +1,29 @@
 variable "path" {
   default = "/Users/emilseverinramsvik/Documents/twitter-note-publisher/infra/"
+  description = "Path to the project terrafrom directory"
 }
 
 variable "AIRTABLE_API_KEY" {
   sensitive = true
+  default = "API key to the airtable database"
 }
 variable "TWITTER_ACCESS_TOKEN_SECRET" {
   sensitive = true
+  description = "From Twitter Developer account"
 }
 variable "TWITTER_ACCESS_TOKEN" {
   sensitive = true
+  description = "From Twitter Developer account"
 }
 variable "TWITTER_CONSUMER_SECRET" {
   sensitive = true
+  description = "From Twitter Developer account"
 }
 variable "TWITTER_CONSUMER_KEY" {
   sensitive = true
+  description = "From Twitter Developer account"
 }
 variable "GOOGLE_APPLICAION_CREDENTIALS" {
   sensitive = true
+  description = "JSON credentials for the service account"
 }
